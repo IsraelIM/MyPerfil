ff26bddd186c4572b398b9ea62c0dc63c0783a7c
TeteMorais <teresamorais005@gmail.com>
2023-11-08 15:01:59 +0100

    Iniciando Renponsividade

---
+++ power.js
@@ -0,0 +1,31 @@
+//Menu Flexivel
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
+// Link de Selecção do Menu
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 160;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id +']').classList.add('active');
+            });
+        }
+    });
+    // Navbar Boblavel
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
